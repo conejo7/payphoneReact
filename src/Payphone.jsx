1123683053e3d976f9328a24f1046595283cc1e7
conejo7 <erick_s5490@hotmail.com>
2024-01-30 15:05:35 -0500

    configuracion en payphone saber si se realizo el pago

--- src/Payphone.jsx
+++ src/Payphone.jsx
@@ -19,51 +19,40 @@ const Payphone = () => {
     // const urlConfirm = `https://pay.payphonetodoesposible.com/api/button/V2/Confirm`;
     const authToken = "zp2hBfw6R8EVypUHlBLOffNZKYFWdTxB8XCvOIAEN0TXqEonb71p14PD5PgNwWka4Rjex_tqteF_LnhvdXW1cz639OQMDQPigu7VQuYdx_PPC06NnsJI4lKp3g5f45zPTYGNvfNJiOCDooY2Lruwa3-oA2eAGatBWuqq0_IETWg_XHkQmCTO1Sx1xgPGqIE_1hDWokkscokCN2RVuarhd1NVO2FZA1WmSsIYXo80mRlGb1vtEQyxwzhCNuyuoG_T2DXTh4j7H0dIW9z3AQ0-jShfFlWFJBTTHZRujiTxj3VsSnMkZ-8JXqALzXdEx4SRyGFONw";
     // const [dataId, setDataId] = useState('');
-
-
-
-    useEffect(() => {
-        // Obtener parámetros de la URL en React
-        const urlParams = new URLSearchParams(window.location.search);
-        const transaccion = urlParams.get('id');
-        const client = urlParams.get('clientTransactionId');
-        console.log(transaccion);
-        console.log(client);
-
-        // Preparar el objeto de datos JSON
-        const data = {
-            id: parseInt(transaccion, 10),
-            clientTxId: client,
-        };
-
-        // Realizar la solicitud con axios
-        axios.post('https://pay.payphonetodoesposible.com/api/button/V2/Confirm', data, {
-            headers: {
-                'Authorization': `Bearer ${authToken}`
-            }
-        })
-            .then((response) => {
-                // Manejar la respuesta aquí, response.data contiene la respuesta JSON
-                console.log(response.data);
-                console.log(response.data.transactionStatus);
-                // setDataId(response.data.transactionStatus);
-            })
-            .catch((error) => {
-                // Manejar errores aquí
-                console.error('Error en la solicitud:', error);
-            });
-    }, []); // El array vacío [] asegura que useEffect se ejecute solo una vez al montar el componente
+    // useEffect(() => {
+    //     // Obtener parámetros de la URL en React
+    //     const urlParams = new URLSearchParams(window.location.search);
+    //     const transaccion = urlParams.get('id');
+    //     const client = urlParams.get('clientTransactionId');
+    //     console.log(transaccion);
+    //     console.log(client);
+    //     // Preparar el objeto de datos JSON
+    //     const data = {
+    //         id: parseInt(transaccion, 10),
+    //         clientTxId: client,
+    //     };
+    //     // Realizar la solicitud con axios
+    //     axios.post('https://pay.payphonetodoesposible.com/api/button/V2/Confirm', data, {
+    //         headers: {
+    //             'Authorization': `Bearer ${authToken}`
+    //         }
+    //     })
+    //         .then((response) => {
+    //             // Manejar la respuesta aquí, response.data contiene la respuesta JSON
+    //             console.log(response.data);
+    //             console.log(response.data.transactionStatus);
+    //             // setDataId(response.data.transactionStatus);
+    //         })
+    //         .catch((error) => {
+    //             // Manejar errores aquí
+    //             console.error('Error en la solicitud:', error);
+    //         });
+    // }, []); // El array vacío [] asegura que useEffect se ejecute solo una vez al montar el componente
 
 
     const onClick = () => {
-
-
         const url = `https://pay.payphonetodoesposible.com/api/button/Prepare`;
-
         console.log("url" + url);
-
-        // Aquí puedes enviar el canvasImageURL a tu API para guardar la imagen en el servidor
-        // Ejemplo de cómo hacer una solicitud a la API (requiere biblioteca "axios"):
         axios.post(url, jsonData, {
             headers: {
                 'Authorization': `Bearer ${authToken}`
@@ -71,24 +60,50 @@ const Payphone = () => {
         })
             .then((response) => {
                 const payWithCardUrl = response.data.payWithCard;
-
-                // Redirigir al usuario a la URL de redirección
                 window.location.href = payWithCardUrl;
                 alert("Boton de pago");
                 console.log('Imagen guardada con éxito en el servidor.');
-                dispatch( startNewSubscription() );
+
+                //si el pago fue exitoso
+                const urlParams = new URLSearchParams(window.location.search);
+                const transaccion = urlParams.get('id');
+                const client = urlParams.get('clientTransactionId');
+                console.log(transaccion);
+                console.log(client);
+                // Preparar el objeto de datos JSON
+                const data = {
+                    id: parseInt(transaccion, 10),
+                    clientTxId: client,
+                };
+                // Realizar la solicitud con axios
+                axios.post('https://pay.payphonetodoesposible.com/api/button/V2/Confirm', data, {
+                    headers: {
+                        'Authorization': `Bearer ${authToken}`
+                    }
+                })
+                    .then((response) => {
+                        // Manejar la respuesta aquí, response.data contiene la respuesta JSON
+                        console.log(response.data);
+                        console.log(response.data.transactionStatus);
+                        // setDataId(response.data.transactionStatus);
+                    })
+                    .catch((error) => {
+                        // Manejar errores aquí
+                        console.error('Error en la solicitud:', error);
+                    });
 
             })
             .catch((error) => {
                 console.error('Error al guardar la imagen en el servidor.', error);
-
             });
+
+
+
 
     }
 
     return (
         <div>
-
             <div id="pp-button"></div>
             <IconButton
                 onClick={onClick}
